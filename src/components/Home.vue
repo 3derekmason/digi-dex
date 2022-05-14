3d27dc296c0b680edb92ecf9673af0f151737285
Derek Mason <3derekmason@gmail.com>
2022-05-14 16:19:51 -0600

    link to api

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,20 @@
 <template>
   <v-container>
     <h1>Digi-Dex</h1>
+    <v-btn v-on:click="getAllDigis">Test</v-btn>
   </v-container>
 </template>
 
 <script>
 export default {
   name: "HomePage",
+  data() {},
+  methods: {
+    getAllDigis: async () => {
+      const res = await fetch("https://digimon-api.vercel.app/api/digimon");
+      const data = await res.json();
+      console.log(data);
+    },
+  },
 };
 </script>
