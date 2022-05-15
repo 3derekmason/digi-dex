97d657b296edbe8d8f13157919873d8513208de8
Derek Mason <3derekmason@gmail.com>
2022-05-14 21:28:45 -0600

    fetch and display img for digi

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,36 @@
 <template>
   <v-container>
     <h1>Digi-Dex</h1>
-    <v-btn v-on:click="getAllDigis">Test</v-btn>
+    <v-btn v-on:click="getAllDigis">Fetch</v-btn>
+    <v-btn v-on:click="showDigis">Show</v-btn>
+    <div class="digiView">
+      <img v-for="digi in digimon" v-bind:key="digi" :src="digi.img" />
+    </div>
   </v-container>
 </template>
 
 <script>
 export default {
   name: "HomePage",
-  data() {},
+  data() {
+    return {
+      digimon: [],
+    };
+  },
   methods: {
-    getAllDigis: async () => {
+    async getAllDigis() {
       const res = await fetch("https://digimon-api.vercel.app/api/digimon");
       const data = await res.json();
-      console.log(data);
+      data.forEach((digi) => {
+        this.digimon.push(digi);
+        console.log("fetched");
+      });
+    },
+    showDigis() {
+      console.log(this.digimon);
+    },
+    getPhoto() {
+      return this.img;
     },
   },
 };
