d7fa2e0f9142a8d524aba0485f06b4e5236f6f6a
Derek Mason <3derekmason@gmail.com>
2022-05-17 09:10:18 -0600

    add select to toolbar

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="toolbar"></div>
+  <div class="toolbar">
+    <v-select :items="attributes" label="Order By:" class="d-select"></v-select>
+  </div>
 </template>
 
 <script>
 export default {
   name: "MainToolbar",
+  data: () => {
+    return {
+      attributes: ["Level", "Name"],
+    };
+  },
   props: {
     digimon: Array,
   },
@@ -13,10 +20,15 @@ export default {
 
 <style scoped>
 .toolbar {
+  padding: 0;
   margin-top: 8px;
-  width: 80%;
-  height: 64px;
+  width: 20%;
+  height: 56px;
   background: #a9aabc;
   border-radius: 4px;
+}
+.d-select {
+  border-radius: 4px;
+  text-align: center;
 }
 </style>
